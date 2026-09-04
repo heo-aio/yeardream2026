e2725cc0daf8d44e36c4f7a770840a4b7c8be195
kim,ji-hoon <jihookuku@gmail.com>
2026-09-04 11:46:11 +0900

    스트리밍 테스트

--- huggingface/source/12_answer.py
+++ huggingface/source/12_answer.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 
 model_id = 'Qwen/Qwen2.5-1.5B-Instruct'
 # 1. 토크나이저
@@ -15,17 +15,42 @@ model = AutoModelForCausalLM.from_pretrained(
 prompt = input('AI 에게 질문하고 싶은 내용은?\n')
 print(prompt)
 # 3. 토큰화
-inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+message = [
+    {"role":"system","content":"너는 IT 전문가야, AI 지식을 주로 다루고 있으며 알기쉽게 예를 들어서 설명해주는것을 잘해"},
+    {"role":"user","content":prompt}
+]
+
+chat = tokenizer.apply_chat_template(
+    message,
+    tokenize=False,
+    add_genderation_prompt=True
+)
+print(chat)
+
+inputs = tokenizer(chat, return_tensors="pt").to(model.device)
 
 # 4. 추론
 print("생각 하는 중...")
+# with torch.no_grad():
+#     output = model.generate(
+#         **inputs,
+#         max_new_tokens=2048,
+#         do_sample=True,
+#         temperature=0.7,
+#         eos_token_id=tokenizer.eos_token_id
+#     )
+#     print(tokenizer.decode(output[0]))
+
+# 실시간 출력을 위해서는 Streamer 가 필요하다.
+streamer = TextStreamer(tokenizer,skip_prompt=True)
+
 with torch.no_grad():
-    output = model.generate(
+    model.generate(
         **inputs,
         max_new_tokens=2048,
         do_sample=True,
         temperature=0.7,
-        eos_token_id=tokenizer.eos_token_id
+        eos_token_id=tokenizer.eos_token_id,
+        streamer=streamer
     )
-    print(tokenizer.decode(output[0]))
 
