705149371f6223b115939524c8bf70ddb3d1c2d4
kim,ji-hoon <jihookuku@gmail.com>
2026-09-04 11:16:01 +0900

    모델변경

--- huggingface/source/12_answer.py
+++ huggingface/source/12_answer.py
@@ -1,31 +1,31 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_id = 'LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct'
+model_id = 'Qwen/Qwen2.5-1.5B-Instruct'
 # 1. 토크나이저
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 # 2. 모델
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
     trust_remote_code=True,
-    dtype=torch.bfloat16,
+    dtype=torch.float16,
     device_map="auto"
 )
 
 prompt = input('AI 에게 질문하고 싶은 내용은?\n')
 print(prompt)
+# 3. 토큰화
 inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
+# 4. 추론
 print("생각 하는 중...")
 with torch.no_grad():
     output = model.generate(
         **inputs,
-        max_new_tokens=246,
+        max_new_tokens=2048,
         do_sample=True,
         temperature=0.7,
         eos_token_id=tokenizer.eos_token_id
     )
     print(tokenizer.decode(output[0]))
 
-# 3. 토큰화
-# 4. 추론
