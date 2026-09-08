2351ec59c86c464d2ecd867ac53dd61f1998f4ce
kim,ji-hoon <jihookuku@gmail.com>
2026-09-08 11:40:54 +0900

    train_api 완료

--- huggingface/usage/07_train_api.py
+++ huggingface/usage/07_train_api.py
@@ -1,7 +1,8 @@
 import evaluate
 import numpy as np
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, \
+    DataCollatorWithPadding
 
 # 1. 토크나이저 로드
 model_id = "bert-base-uncased"
@@ -40,12 +41,46 @@ def main():
     # print(token_ds)
 
     # 3. 모델 생성
+    model = AutoModelForSequenceClassification.from_pretrained(model_id, num_labels=2)
     # 4. 학습 상세정보 지정(하이퍼 파라메터)
+    args = TrainingArguments(
+        output_dir='./results', # 결과와 체크포인트 저장 경로
+        num_train_epochs=2,     # 전체 학습 수
+        per_device_train_batch_size=16, # 학습당 묶음 수
+        per_device_eval_batch_size=16,  # 평가당 묶음 수
+        learning_rate=0.00002,          # 보폭(2e-5)
+        eval_strategy="epoch",          # 평가 시점 - 학습당
+        save_strategy="epoch",              # 저장 시점 - 학습당
+        load_best_model_at_end=True,        # 평가가 끝난 시점에서 가장 우수한 모델을 로드
+        metric_for_best_model="accuracy",   # 최고의 모델을 판단하는 매트릭 기준
+        logging_steps=100,                  # 100번의 step마다 로그 출력
+        report_to="none",                   # 외부 로깅툴 사용 여부
+        dataloader_num_workers= 4           # 데이터 로딩시 사용할 스레드/프로세스 수
+    )
     # 5. Trainer API 생성
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=token_ds['train'],
+        eval_dataset=token_ds['test'],
+        processing_class=tokenizer,
+        data_collator= DataCollatorWithPadding(tokenizer=tokenizer), # 토크나이징 후 패딩설정
+        compute_metrics= compute_matrics    #평가지표함수 지정
+    )
+
     # 6. 학습
+    print('모델학습 진행 중...')
+    trainer.train()
+
     # 7. 평가
+    results = trainer.evaluate()
+    print(f'평가 결과 : {results}')
+
     # 8. 저장
-    pass
+    save_path='./my_imbd_bert_model'
+    trainer.save_model(save_path)
+    tokenizer.save_pretrained(save_path)
+    print(f'모델저장완료!!')
 
 # 메인 프로세서(스레드) 만 진입해서 실행해라
 if __name__ == '__main__':
