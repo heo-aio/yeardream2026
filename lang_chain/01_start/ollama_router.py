9d59a867203cf63e303cc4d5be80a931a37bfe89
kim,ji-hoon <jihookuku@gmail.com>
2026-09-10 11:19:31 +0900

    스트리밍 완성

--- lang_chain/01_start/ollama_router.py
+++ lang_chain/01_start/ollama_router.py
@@ -1,7 +1,12 @@
+from typing import Dict
+
+from fastapi import APIRouter
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
+from starlette.responses import StreamingResponse
 
+router = APIRouter(prefix="/ask", tags=["ask"])
 model = ChatOllama(model="exaone3.5:2.4b")
 
 prompt = ChatPromptTemplate.from_messages([
@@ -9,9 +14,16 @@ prompt = ChatPromptTemplate.from_messages([
     ("user","{topic} 에 대해서 설명해 주세요")
 ])
 
-q = input("질문 내용을 입력 하세요:\n")
+@router.post("/stream")
+def get_answer(info:Dict[str,str]):
+    # content 부분은 지속적으로 데이터를 줘야 한다.
+    # return 은 여러번 할 수 없다.
+    # StreamingResponse 안에서 지속적으로 실행며 데이터를 줄 함수가 필요하다.
+    return StreamingResponse(output_str(info['q']),media_type="text/plain")
 
-chain = prompt|model|StrOutputParser()
 
-for chunk in chain.stream({"topic":q}):
-    print(chunk,end='',flush=True)
+def output_str(q):
+    chain = prompt|model|StrOutputParser()
+    for chunk in chain.stream({"topic":q}):
+        # print(chunk,end='',flush=True)
+        yield chunk # return 후 완전히 종료된게 아니면 대기한다.
