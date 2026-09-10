bbf2e674706ee01f22eb9a3374c1d17f04c2a31e
kim,ji-hoon <jihookuku@gmail.com>
2026-09-10 15:53:37 +0900

    채팅서비스 완성

--- lang_chain/03_chat/main.py
+++ lang_chain/03_chat/main.py
@@ -1,33 +1,19 @@
-from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_ollama import ChatOllama
+from typing import Dict
 
-# 모델 호출
-model = ChatOllama(model="exaone3.5:2.4b")
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse, StreamingResponse
+from starlette.staticfiles import StaticFiles
 
-conversation_history = [] # 대화저장 리스트
+from ollama_service import chat_answer
 
-# 프롬프트 작성
-prompt = ChatPromptTemplate.from_messages([
-    ("system","당신은 답변 전문 AI 모델 입니다. 주워진 질문에 대해서 핵심만 간단히 대답하세요"),
-    MessagesPlaceholder(variable_name="history"), # 대화내용을 history 라는 이름으로 줄께
-    ("user","{query}")
-])
+app = FastAPI()
+app.mount("/view",StaticFiles(directory="view"))
 
-chain = prompt|model # 파이프라인 조립
+@app.get("/")
+def main():
+    return RedirectResponse("/view/chat.html")
 
-# 실행 및 출력
-while True:
-    query = input('\n당신> ')
-    if query == '/exit' or query == '/bye':
-        print('대화를 종료 합니다.')
-        break
-    answer = ''
-    for chunk in chain.stream({'query':query,'history':conversation_history}):
-        print(chunk.content,end='', flush=True) # StrOutputParser() 안써서 .content 붙이는 것
-        answer += chunk.content
-
-    conversation_history.append(HumanMessage(content=query))
-    conversation_history.append(AIMessage(content=answer))
-    print()
-    print(f'history length : {len(conversation_history)}')
+@app.post("/ask/chat")
+def ask_chat(info:Dict[str,str]):
+    print(f'input : {info['q']}')
+    return StreamingResponse(chat_answer(info['q']), media_type="text/plain")
