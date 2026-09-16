534775fdce37c0b102b9e2113a6eb72885769462
kim,ji-hoon <jihookuku@gmail.com>
2026-09-16 10:50:53 +0900

    최종 완성

--- lang_graph/source/04_loop.py
+++ lang_graph/source/04_loop.py
@@ -73,7 +73,15 @@ def critic_node(state:WriteState) -> WriteState:
 
 def route_by_review(state:WriteState) -> str:
     """PASS / RETRY 에 따라서 다른 노드로 갈수있는  문자열을 반환"""
-    return "go_retry"
+    if state.state == 'PASS':
+        print('검증 통과')
+        return "go_end"
+    elif state.count >= 3:
+        print('3회 초과로 재시도 중지')
+        return "go_end"
+    else:
+        print(f'{state.count} 회 시도 \n 피드백 : {state.feedback}')
+        return "ge_retry"
 
 wf = StateGraph(WriteState)# 4. 저장소 등록
 # 5. 노드 등록
@@ -82,9 +90,23 @@ wf.add_node('critic',critic_node)
 # 6. 엣지등록(조립)
 wf.set_entry_point('writer')
 wf.add_edge('writer','critic') # MAC : ^ + space
-wf.add_edge('critic',END)
+wf.add_conditional_edges(
+    'critic',
+    route_by_review,
+    {
+        'go_end':END,
+        "go_retry":'writer'
+    })
 # 7. 컴파일
 app = wf.compile()
 # 8. 실행
-result = app.invoke({'topic':'전기 자동차'})
-print(result)
+for node in app.stream({'topic':'전기 자동차'},stream_mode="updates"):
+    for key,val in node.items():
+        print(f'{key}:{val}')
+
+
+
+
+
+
+
