b93c533ff92a008975088e8dd67f514215c0200f
kim,ji-hoon <jihookuku@gmail.com>
2026-09-16 10:19:21 +0900

    검증노드 완성

--- lang_graph/source/04_loop.py
+++ lang_graph/source/04_loop.py
@@ -44,6 +44,26 @@ def write_node(state:WriteState) -> WriteState:
 
 def critic_node(state:WriteState) -> WriteState:
     """카피를 검증하고 승인여부와 피드백을 반환하는 노드"""
+    # 합격조건 : 혁신 또는 미래라는 키워드가 반드시 들어가야 함
+    prompt = f"""
+        당신은 깐깐한 카피라이트 검수자 입니다. 진부한 카피를 걸러냅니다.
+        다음의 [광고카피]를 평가해 주세요.
+        [광고카피] : {state.draft}
+
+        [합격조건]
+        1. 카피 내 '혁신' 또는 '미래' 라는 키워드가 반드시 포함되어야 함
+        2. 미래 지향적인 내용이어야 함
+
+        [출력조건]
+        다른설명 필요 없이 아래 형태의 JSON 포맷으로 응답해야함
+        ``` 등의 JSON 에 불필요한 문자는 모두 제외
+        {{
+        "state":"오직 PASS 또는 RETRY 만 표기",
+        "feedback":"state 가 RETRY 일 경우 조건을 만족하지 못하는 이유, PASS 일 경우 칭찬"
+        }}
+    """
+    resp = llm.invoke(prompt)
+    print(resp.content)  # JSON 형태만 깔끔하게 잘 나오는가?
     return state
 
 def route_by_review(state:WriteState) -> str:
@@ -53,9 +73,11 @@ def route_by_review(state:WriteState) -> str:
 wf = StateGraph(WriteState)# 4. 저장소 등록
 # 5. 노드 등록
 wf.add_node('writer',write_node)
+wf.add_node('critic',critic_node)
 # 6. 엣지등록(조립)
 wf.set_entry_point('writer')
-wf.add_edge('writer',END) # MAC : ^ + space
+wf.add_edge('writer','critic') # MAC : ^ + space
+wf.add_edge('critic',END)
 # 7. 컴파일
 app = wf.compile()
 # 8. 실행
