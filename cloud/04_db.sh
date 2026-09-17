46dd557146cc2b52f524caabbb51ea73ba0ebb79
kim,ji-hoon <jihookuku@gmail.com>
2026-09-17 15:43:04 +0900

    관리자계정설정

---
+++ cloud/04_db.sh
@@ -0,0 +1,19 @@
+# mariadb 설치
+sudo yum install -y mariadb1011-server mariadb1011
+
+# mariadb 실행
+sudo systemctl start mariadb
+# 서비스 등록(서버 켜지면 무조건 같이 켜지도록)
+sudo systemstl enable mariadb
+
+# 상태 확인
+sudo systemstl status mariadb
+
+# 관리자(root) 계정 설정
+sudo mysql_secure_installation
+# Switch to unix_socket authentication:n # 비번 없이 DB 접속 여부
+# Change the root password?:n # 비번 변경 여부
+# Remove anonymous users?:Y # 익명유저 삭제여부
+# Disallow root login remotely? Y (원격 root 접속 제한)
+# Remove test database and access to it? Y (테스트 DB 삭제)
+# Reload privilege tables now? Y (권한 적용)
