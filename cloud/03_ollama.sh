05b48f49845d9f966f977094d00c72993a8c479f
kim,ji-hoon <jihookuku@gmail.com>
2026-09-17 15:10:36 +0900

    서비스 올리기 완료

--- cloud/03_ollama.sh
+++ cloud/03_ollama.sh
@@ -23,3 +23,20 @@ Environment="OLLAMA_HOST=0.0.0.0"
 # Ollama 재시동
 sudo systemctl daemon-reload
 sudo systemctl restart ollama
+
+# 소스 이동(main.py, ollama_service.py,view,requirements.txt)
+vim ollama_service.py
+# 모델명 수정 후 ESC -> :wq
+
+# 가상환경 생성
+python3.14 -m venv .venv
+# 가상환경 실행
+source venv/bin/activate
+
+# requirements.txt 설치
+pip install --upgrade pip
+pip install uv
+uv pip install requirements.txt
+
+# 서버시작
+uvicorn main:app --host=0.0.0.0 port=8000
