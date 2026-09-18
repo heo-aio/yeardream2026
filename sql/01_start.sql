94a62302b346d6838288963f8e60f8407784c3d6
kim,ji-hoon <jihookuku@gmail.com>
2026-09-18 11:02:57 +0900

    테이블 다루기(DDL)

--- sql/01_start.sql
+++ sql/01_start.sql
@@ -60,9 +60,45 @@ DESC test_table;
 -- mobile 문자열 11자
 -- salary 숫자형 8자
 -- reg_date 날짜 기본으로 입력 된다.
+CREATE TABLE employees(
+	emp_no INT(3),
+	first_name VARCHAR(8),
+	last_name VARCHAR(8),
+	email VARCHAR(50),
+	mobile VARCHAR(11),
+	salary INT(8),
+	reg_date DATE DEFAULT CURDATE()
+);
+desc employees; -- 테이블 구조 확인
+-- database 내 테이블 목록
+show tables;
 
+-- 테이블 삭제
+DROP TABLE test_table;
 
+-- 테이블 수정
+-- 1) 컬럼 이름 수정
+-- ALTER TALBE [테이블 이름] RENAME COLUMN [변경전이름] TO [변경 후 이름]
+ALTER TABLE employees RENAME COLUMN last_name TO family_name;
+DESC employees;
 
+-- 2) 컬럼 추가
+-- ALTER TALBE [테이블이름] ADD ([컬럼명] [데이터타입](사이즈));
+ALTER TABLE employees ADD (depart_no VARCHAR(10));
+ALTER TABLE employees ADD (commission VARCHAR(10));
+ALTER TABLE employees ADD (etc VARCHAR(100));
+DESC employees;
+
+-- 3) 컬럼 속성 변경
+-- ALTER TABLE [테이블이름] MODIFY COLUMN [컬럼명] [데이터타입](사이즈);
+-- 00.00 의 경우 float(전체자리수,소숫점자리수)
+ALTER TABLE employees MODIFY COLUMN commission float(4,2);
+DESC employees;
+
+-- 4) 컬럼 삭제
+-- ALTER TABLE [테이블명] DROP COLUMN [컬럼명];
+-- etc 컬럼 삭제
+ALTER TABLE employees DROP COLUMN etc;
 
 
 
