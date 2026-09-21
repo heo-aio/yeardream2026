a2851e46f7c6d30d18f162583afc3e26878e6732
kim,ji-hoon <jihookuku@gmail.com>
2026-09-21 14:36:17 +0900

    서브쿼리

--- sql/07_sub_query.sql
+++ sql/07_sub_query.sql
@@ -39,13 +39,72 @@ SELECT * FROM emp;
 
 -- 문제1> han 의 근무 부서 이름?
 -- 3+4 = 7
+-- 이름이 han 인 직원의 부서 번호 알아내기
 SELECT deptno FROM emp WHERE ename = 'han';
 -- 7*2 = 14
+-- 해당 부서 번호로 부서의 이름 알아내기
 SELECT deptname FROM dept WHERE deptno = 1;
 
 -- 7 * (3+4) = 14
 SELECT deptname FROM dept 
 	WHERE deptno = (SELECT deptno FROM emp WHERE ename = 'han');
+
+
+-- 문제 2> 부서위치가 LA 또는 BOSTON 인 부서에 속한 사람들의 이름과 직책
+-- 부서위치가 LA 또는 BOSTON 의 번호
+
+-- 부서번호를 통해서  이름, 직책
+SELECT ename,job FROM emp WHERE deptno IN (2,4);
+
+SELECT ename,job FROM emp 
+	WHERE deptno IN (SELECT deptno FROM dept WHERE loc IN('LA','BOSTON'));
+
+
+-- 문제 3> sales 부서에 근무하는 사원의 이름,직책,입사일 알아보기
+-- sales 부서의 deptno 알아보기
+SELECT deptno FROM dept WHERE deptname = 'SALES';
+-- deptno = 1 인 사람의 이름, 직책, 입사일 찾기
+SELECT ename,job,hiredate FROM emp WHERE deptno = 1;
+
+SELECT ename,job,hiredate FROM emp 
+	WHERE deptno = (SELECT deptno FROM dept WHERE deptname = 'SALES');
+
+-- 문제 4> 직책이 MANAGER 인 직원들(여러명일 경우 가장 빠른사람 기준) 보다 입사일이 빠른 사람들
+-- 이름,직책,입사일
+SELECT hiredate FROM emp WHERE job = 'manager' ORDER BY hiredate LIMIT 1; -- '2025-08-12'
+SELECT ename,job,hiredate FROM emp WHERE hiredate < '2025-08-12';
+
+SELECT ename,job,hiredate FROM emp 
+	WHERE hiredate < (SELECT MIN(hiredate) FROM emp WHERE job = 'manager');
+
+-- 문제 5> 부서별로 직원이 몇명인지 알려주세요
+SELECT * FROM dept; -- 1,2,3,4
+
+SELECT deptname FROM dept WHERE deptno = 1;
+
+-- 서브쿼리가 메인쿼리의 일부가 되었다.
+-- 상하 관계 쿼리
+-- dept 의 deptno 가 오직 1만 나타남
+SELECT 	
+	deptname ,
+	(SELECT COUNT(deptno) FROM emp WHERE deptno = 1) as cnt
+FROM dept WHERE deptno = 1;
+
+SELECT * FROM dept;
+
+SELECT 	
+	deptname ,
+	-- 그렇게 불러온 deptno 를 활용함
+	(SELECT COUNT(deptno) FROM emp WHERE deptno = dept.deptno) as cnt
+FROM dept; -- dept 의 모든 deptno 를 불러옴
+
+
+
+
+
+
+
+
 
 
 
